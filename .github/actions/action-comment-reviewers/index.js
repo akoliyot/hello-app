1981d9a4fa3f8550d4e89f06ea2688822b77606b
Aswin Koliyot <akoliyot@gmail.com>
2021-04-02 15:50:56 +0200

    Setup octokit to write comment

--- .github/actions/action-comment-reviewers/index.js
+++ .github/actions/action-comment-reviewers/index.js
@@ -1,5 +1,6 @@
 const core = require("@actions/core");
 const github = require("@actions/github");
+const { Octokit } = require("@octokit/action");
 
 try {
   const nameToGreet = core.getInput("who-to-greet");
@@ -10,6 +11,19 @@ try {
 
   const payload = JSON.stringify(github.context.payload, undefined, 2);
   console.log(`The event payload: ${payload}`);
+
+  // Comment that a reviewer has been assigned.
+  const githubToken = core.getInput("GITHUB_TOKEN");
+  const context = github.context;
+  const prNumber = context.payload.pull_request.number;
+  const message = "Reviewer has been notified";
+
+  const octokit = new Octokit();
+  octokit.issues.createComment({
+    ...context.repo,
+    issue_number: prNumber,
+    body: `Aha! ${message}`,
+  });
 } catch (error) {
   core.setFailed(error.message);
 }
